f4c9c57bcc44e68ec50a7e0cafaca153c092a988
Michael Sanchez <secti0n33@live.com>
2019-11-25 21:08:59 -0500

    Added semi-functional purchase area

--- scripts.js
+++ scripts.js
@@ -5,6 +5,9 @@ function formchange()
     var book=document.getElementById("Dropdown").value;
     document.getElementById("Price").innerHTML=(book=="Math book") ? 
     "$35" : (book=="Science book") ? "$25" : (book=="English book") ? "$12" : (book=="Computer Science book") ?"$3" : "";
+    if(book!="Choose a book")
+        document.getElementById("Submit").disabled=false;
+    return book;
 }
 
 var form2 = () =>
@@ -13,7 +16,15 @@ var form2 = () =>
     console.log(v);
     document.getElementById("swap").innerHTML=
     (v==0)? "Last Name: <input type=\"text\" id=\"ln\"><br>\
-    First Name: <input type=\"text\" id=\"First\"><br>":
+    First Name: <input type=\"text\" id=\"First\"><br>\
+    <select id=\"cardtype\"><option>MasterCard</option><option>Visa</option></select><br>\
+    Card Number: <input type=\"number\" id=\"cn\"><br>\
+    Security Code: <input type=\"number\"><br>\
+    Billing Address: <input type=\"text\"><br>\
+    Zip Code: <input type=\"number\"><br>\
+    State: <input type=\"text\"><br>\
+    City/Town: <input type=\"text\"><br>\
+    <button id=\"Submit\" "+((formchange()!="Choose a book")? "":"disabled=\"true\"")+">Submit</button>":
     (v==1)? "<h5>Email <a href=\"mailto:ducklend@gmail.com\">DuckLend@gmail.com</a> to find out if your<br>payment method is acceptable</h5>":
     "Select payment method:<br>\
     <input type=\"radio\" name=\"payment\" value=0>Debit/Credit<br>\
